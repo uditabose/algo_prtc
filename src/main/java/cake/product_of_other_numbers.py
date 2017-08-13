7a305694e689b88cf80c7550dd429a6f1a71f63b
Udita Bose <ub298@nyu.edu>
2017-08-13 19:40:12 -0400

    cake : 3

--- src/main/java/cake/product_of_other_numbers.py
+++ src/main/java/cake/product_of_other_numbers.py
@@ -1,4 +1,9 @@
 def product_of_other_numbers_with_division(numbers):
+    '''
+    https://www.interviewcake.com/question/python/product-of-other-numbers
+    :param numbers:
+    :return:
+    '''
     all_mult = reduce(lambda x, y : x * y, numbers)
     i = 0
     while i < len(numbers):
@@ -8,7 +13,27 @@ def product_of_other_numbers_with_division(numbers):
     return numbers
 
 def product_of_other_numbers_no_division(numbers):
-    
+    '''
+    https://www.interviewcake.com/question/python/product-of-other-numbers
+    :param numbers:
+    :return:
+    '''
+    left_product = 1
+    product = [1] * len(numbers)
+    i = 0
+    while i < len(numbers):
+        product[i] = left_product
+        left_product *= numbers[i]
+        i += 1
+
+    right_product = 1
+    i = len(numbers) - 1
+    while i > -1:
+        product[i] *= right_product
+        right_product *= numbers[i]
+        i -= 1
+    return product
 
 if __name__ == '__main__':
     print("Other %s" % product_of_other_numbers_with_division([1, 7, 3, 4]))
+    print("Other %s" % product_of_other_numbers_no_division([1, 7, 3, 4]))
