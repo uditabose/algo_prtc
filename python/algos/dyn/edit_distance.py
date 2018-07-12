b33656e3d64726c6a1dad92c1b521e2d65261e1d
ub298 <ub298@nyu.edu>
2018-07-12 00:27:27 -0400

    algo : dyn, edit dist

--- python/algos/dyn/edit_distance.py
+++ python/algos/dyn/edit_distance.py
@@ -9,9 +9,16 @@ def edit_distance(astr, bstr):
 
     for i in range(0, len(astr)):
         for j in range(0, len(bstr)):
-            if astr[i] == bstr[]:
-                pass
-
+            if i == 0:
+                edit_matrix[i][j] = j
+            elif j == 0:
+                edit_matrix[i][j] = i
+            elif astr[i] == bstr[j]:
+                edit_matrix[i][j] = edit_matrix[i-1][j-1]
+            else:
+                edit_matrix[i][j] = 1 + min(edit_matrix[i-1][j-1], edit_matrix[i-1][j] , edit_matrix[i][j-1] )
+                
+    return edit_matrix[len(astr-1)][len(bstr-1)]
 
 if __name__ == '__main__':
     print(edit_distance('snowy', 'sunny'))
